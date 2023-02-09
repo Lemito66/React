38aecfe63e0f0903f858a32dfa05238b20546f94
Lemito66 <lemito66@gmail.com>
2023-02-09 17:58:01 -0500

    Añadir componente de Twitter

--- projects/HolaMundo/src/main.jsx
+++ projects/HolaMundo/src/main.jsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import HolaMundo  from './HolaMundo'
+import TwitterCard from './TwitterCard'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
     {/* <App /> */}
     <HolaMundo />
+    <TwitterCard 
+      name="Emill"
+      username="lemito66"
+      isFollowing={true}
+    />
+    <TwitterCard 
+      name="Emill"
+      username="lemito66"
+      isFollowing={false}
+    />
   </React.StrictMode>,
 )
