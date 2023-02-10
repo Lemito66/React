7479cc300180d9f053b595ae74cef060e74e80fa
Lemito66 <lemito66@gmail.com>
2023-02-10 12:09:35 -0500

    cambio de formato por prettier

--- projects/HolaMundo/src/TwitterCard.jsx
+++ projects/HolaMundo/src/TwitterCard.jsx
@@ -1,31 +1,47 @@
 import { useState } from "react";
 
-function TwitterCard ({name, username, }){
+function TwitterCard({ name, username }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followers, setFollowers] = useState(0);
 
-    const [isFollowing, setIsFollowing] = useState(false);
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+  const sumarFollowers = () => {
+    setFollowers(followers + 1);
+  };
 
-    const handleClick = () => {
-        setIsFollowing(!isFollowing);
+  const restarFollowers = () => {
+    if (followers <= 0) {
+      alert("No puedes tener menos de 0 seguidores");
+    } else {
+      setFollowers(followers - 1);
     }
+  };
 
-    const imageSrc = `https://unavatar.io/${username}`;
-    const isFollowingText = isFollowing ? 'Siguiendo' : 'Seguir';
-    return(
-        <article>
-            <header>
-                <img alt="Avatar de reddit" src={imageSrc}></img>
-                <div>
-                    <strong>{name}</strong><br/>
-                    <span>@{username}</span>
-                </div>
-            </header>
+  const imageSrc = `https://unavatar.io/${username}`;
+  const isFollowingText = isFollowing ? "Siguiendo" : "Seguir";
+  return (
+    <article>
+      <header>
+        <img alt="Avatar de reddit" src={imageSrc}></img>
+        <div>
+          <strong>{name}</strong>
+          <br />
+          <span>@{username}</span>
+        </div>
+      </header>
 
-            <aside>
-                <button onClick={handleClick}>
-                    {isFollowingText}
-                </button>
-            </aside>
-        </article>
-    )
+      <aside>
+        <button onClick={handleClick}>{isFollowingText}</button>
+      </aside>
+      <aside>
+        <button onClick={sumarFollowers}>{followers}</button>
+      </aside>
+      <aside>
+        <button onClick={restarFollowers}>Restar</button>
+      </aside>
+    </article>
+  );
 }
 export default TwitterCard;
