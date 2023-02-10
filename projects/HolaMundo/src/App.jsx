063fc11f652ddf15c285d796c2f044d7bda45f08
Lemito66 <lemito66@gmail.com>
2023-02-10 15:35:52 -0500

    Listo con un arreglo de objetos

--- projects/HolaMundo/src/App.jsx
+++ projects/HolaMundo/src/App.jsx
@@ -1,15 +1,40 @@
 import './App.css'
 import TwitterCard from './TwitterCard'
 
+const users = [
+  {
+    name: "Emill",
+    username: "lemito66",
+    initialIsFollowing: false,
+  },
+  {
+    name: "Emill",
+    username: "reddit.com",
+    initialIsFollowing: false,
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <TwitterCard
+      {
+        users.map((user) => {
+          const { name, username, initialIsFollowing } = user
+          return (
+            <TwitterCard
+              name={name}
+              username={username}
+              initialIsFollowing={initialIsFollowing}
+              key={username}
+            />
+          )
+        })
+      }
+      {/* <TwitterCard
       name="Emill"
       username="lemito66"
       initialIsFollowing={false}
-      />
+      /> */}
 
     </div>
   )
