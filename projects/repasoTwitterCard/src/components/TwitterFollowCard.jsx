858950d1f5a4dcefa2a33c1d2d038dcbdc688a29
Emill <lemito66@gmail.com>
2023-04-06 14:14:51 -0500

    twitter Follow Card

---
+++ projects/repasoTwitterCard/src/components/TwitterFollowCard.jsx
@@ -0,0 +1,33 @@
+import { React, useState } from "react";
+
+export const TwitterFollowCard = ({ user, initialIsFollowing }) => {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+  const handleClick = () => setIsFollowing(!isFollowing);
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          alt="El avatar de Lemito66"
+          src={`https://unavatar.io/${user}`}
+        />
+        <div className="tw-followCard-info">
+          {/* <strong>{children}</strong> */}
+          <span className="tw-followCard-infoUserName">@{user}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
+      </aside>
+    </article>
+  );
+};
