d461819ed9669c8303d46e32fda37605220c78fb
Emill <lemito66@gmail.com>
2023-04-12 16:02:54 -0500

    constantes

---
+++ projects/Tic-Tac-Toe/src/constants.js
@@ -0,0 +1,16 @@
+export const TURNS = {
+  // Creo un objeto con las constantes X y O, que son los turnos
+  X: "❌",
+  O: "⚪",
+};
+export const WINNER_COMBINATIONS = [
+  // Creo un arreglo con todas las combinaciones ganadoras
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
