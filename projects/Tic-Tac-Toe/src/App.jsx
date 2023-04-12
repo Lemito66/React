431cd804e91446240e71f303c63bb47f98f8ccdb
Emill <lemito66@gmail.com>
2023-04-12 12:26:44 -0500

    cambios prettier

--- projects/Tic-Tac-Toe/src/App.jsx
+++ projects/Tic-Tac-Toe/src/App.jsx
@@ -1,84 +1,58 @@
-import './App.css'
-import React, { useState } from 'react'
+import "./App.css";
+import React, { useState } from "react";
 
 const TURNS = {
-  X: 'x',
-  O: 'o',
-}
+  X: "x",
+  O: "o",
+};
 
-
-
-const Square = ({ children, isSelected, updateBoard ,index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-  return (
-    <div className={className}>
-      {children}
-    </div>
-
-  )
-}
-
-
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  return <div className={className}>{children}</div>;
+};
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null)); // Creo un estado para el board
+  const [turn, setTurn] = useState(TURNS.X); // Creo un estado para el turno
 
-  const [board, setBoard] = useState(Array(9).fill(null)) // Creo un estado para el board
-  const [turn, setTurn] = useState(TURNS.X) // Creo un estado para el turno
+  const [number, setNumber] = useState(0); // Creo un estado para el numero
+  const [isFollowing, setIsFollowing] = useState(false); // Creo un estado para el texto
 
-  const [number, setNumber] = useState(0) // Creo un estado para el numero
-  const [isFollowing, setIsFollowing] = useState(false) // Creo un estado para el texto
-
-  const isFollowindText =  isFollowing ? 'Síguiendo' : 'Seguir'
+  const isFollowindText = isFollowing ? "Síguiendo" : "Seguir";
 
   const handleClick = () => {
-    setIsFollowing(!isFollowing)
-  }
+    setIsFollowing(!isFollowing);
+  };
 
   const addNumber = () => {
-    setNumber(number + 1)
-  }
+    setNumber(number + 1);
+  };
 
   return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
 
-      <main className='board'>
-        <h1>Tic Tac Toe</h1>
-        <section className='game'>
-          {
-            board.map((_, index) => {
-              return (
-                <Square 
-                key={index} index={index}>
-                  {board[index]}
-                </Square>
-              )
-            })
-          }
-        </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
 
-        <section className='turn'>
-          <Square 
-          isSelected={turn === TURNS.X}
-          >
-            {TURNS.X}
-          </Square>
-          <Square
-          isSelected={turn === TURNS.O}
-          >
-            {TURNS.O}
-          </Square>
-        </section>
+      <section>
+        <button onClick={addNumber}>{number}</button>
 
-        <section>
-        <button onClick={addNumber}>
-          {number}
-        </button>
-
-        <button onClick={handleClick}>
-          {isFollowindText}
-        </button>
-        </section>
-      </main>
-  )
+        <button onClick={handleClick}>{isFollowindText}</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
