b0a55e158d916cff712ac3d296560138c352bcc9
Lemito66 <lemito66@gmail.com>
2023-11-27 15:51:11 -0500

    url prefijo

--- projects/prueba-tecnica-juniors-trainee/src/api/data.js
+++ projects/prueba-tecnica-juniors-trainee/src/api/data.js
@@ -1,3 +1,3 @@
 export const API_URL_CAT = "https://catfact.ninja/fact";
 
-//export const API_URL_CAT_IMAGE = `https://cataas.com/cat/says/${TEXT}?fontSize=50&fontColor=red`;
+export const CAT_PREFIX_IMAGE_URL = "https://cataas.com/cat/says/";
